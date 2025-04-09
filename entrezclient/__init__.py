4dcb45ff7f35ba0f829c51aacbf758da1884f929
Pieter Provoost <pieterprovoost@gmail.com>
2025-04-09 17:02:38 +0200

    cleanup

--- entrezclient/__init__.py
+++ entrezclient/__init__.py
@@ -2,31 +2,43 @@ from Bio import Entrez, SeqIO
 import time
 from rich.progress import Progress, TextColumn, BarColumn, TimeRemainingColumn, SpinnerColumn, MofNCompleteColumn
 import diskcache as dc
-from multiprocessing import Pool, Lock, Manager
+from multiprocessing import Pool
 import os
 import logging
-from io import StringIO
 import shutil
 
 
 class EntrezClient:
-    def __init__(self, db: str = "nucleotide"):
+    def __init__(self, db: str = "nucleotide", batch_size: int = 1000, num_workers: int = 4, rettype: str = "fasta", retmode: str = "text"):
         self.db = db
-        self.WORK_DIR = 'queue_work'
-        self.PENDING_DIR = 'queue_pending'
-        self.RESULTS_DIR = 'results'
-        self.BATCH_SIZE = 100
-        self.NUM_WORKERS = 2
+        self.work_dir = "queue_work"
+        self.pending_dir = "queue_pending"
+        self.results_dir = "temp_results"
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.rettype = rettype
+        self.retmode = retmode
 
-        self.work = dc.Deque(directory=self.WORK_DIR)
-        self.pending = dc.Deque(directory=self.PENDING_DIR)
+        self.work = dc.Deque(directory=self.work_dir)
+        self.pending = dc.Deque(directory=self.pending_dir)
 
     def search(self, term: str, retmax: int = 1000000000):
-        handle = Entrez.esearch(db=self.db, term=term, retmax=retmax)
-        results = Entrez.read(handle)
-        handle.close()
-        ids = results["IdList"]
-        return ids
+        with Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            MofNCompleteColumn(),
+            "[progress.percentage]{task.percentage:>3.0f}%",
+            TimeRemainingColumn(),
+        ) as progress:
+            task = progress.add_task("[red]Searching", total=1)
+
+            handle = Entrez.esearch(db=self.db, term=term, retmax=retmax)
+            results = Entrez.read(handle)
+            handle.close()
+            ids = results["IdList"]
+            progress.update(task, completed=1)
+            return ids
 
     def recover_pending(self):
         logging.info(f"Recovering {len(self.pending)} pending items")
@@ -36,9 +48,9 @@ class EntrezClient:
     def initialize(self, term: str, retmax: int = 1000000000):
         self.work.clear()
         self.pending.clear()
-        if os.path.exists(self.RESULTS_DIR):
-            shutil.rmtree(self.RESULTS_DIR)
-        os.mkdir(self.RESULTS_DIR)
+        if os.path.exists(self.results_dir):
+            shutil.rmtree(self.results_dir)
+        os.mkdir(self.results_dir)
         ids = self.search(term, retmax=retmax)
         self.work.extend(ids)
 
@@ -59,7 +71,7 @@ class EntrezClient:
             if not ids:
                 break
 
-            logging.info(f"Worker {num} processing {len(ids)} items")
+            logging.debug(f"Worker {num} processing {len(ids)} items")
 
             while True:
                 try:
@@ -69,7 +81,7 @@ class EntrezClient:
                         records = SeqIO.parse(handle, "fasta")
                         count = SeqIO.write(records, out_handle, "fasta")
                     handle.close()
-                    logging.info(f"Worker {num} wrote {count} items to results")
+                    logging.debug(f"Worker {num} wrote {count} items to results")
 
                     for id in ids:
                         pending.remove(id)
@@ -78,15 +90,53 @@ class EntrezClient:
                 except Exception as e:
                     if os.path.exists(os.path.join(results_dir, filename)):
                         os.remove(os.path.join(results_dir, filename))
-                    logging.info(f"Worker {num} encountered exception, waiting for retry: {e}")
+                    logging.debug(f"Worker {num} encountered exception, waiting for retry: {e}")
                     time.sleep(10)
 
-    def download(self, path: str, rettype: str = "fasta", retmode: str = "text"):
+    def download(self, path: str):
         self.recover_pending()
         logging.info(f"Found {len(self.work)} items in work queue")
 
-        time.sleep(5)
+        with Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            MofNCompleteColumn(),
+            "[progress.percentage]{task.percentage:>3.0f}%",
+            TimeRemainingColumn(),
+        ) as progress:
+            total_items = len(self.work) + len(self.pending)
+            task = progress.add_task("[red]Downloading", total=total_items)
 
-        with Pool(processes=self.NUM_WORKERS) as pool:
-            args = [(i, self.WORK_DIR, self.PENDING_DIR, self.RESULTS_DIR, self.BATCH_SIZE, self.db, rettype, retmode) for i in range(self.NUM_WORKERS)]
-            pool.starmap(self.worker, args)
+            with Pool(processes=self.num_workers) as pool:
+                results = []
+                for i in range(self.num_workers):
+                    args = (i, self.work_dir, self.pending_dir, self.results_dir, self.batch_size, self.db, self.rettype, self.retmode)
+                    results.append(pool.apply_async(self.worker, args))
+
+                all_done = False
+                while not all_done:
+                    remaining = len(self.work) + len(self.pending)
+                    completed = total_items - remaining
+                    progress.update(task, completed=completed)
+                    all_done = all(result.ready() for result in results)
+                    time.sleep(10)
+                
+                progress.update(task, completed=total_items)
+
+        with Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            MofNCompleteColumn(),
+            "[progress.percentage]{task.percentage:>3.0f}%",
+            TimeRemainingColumn(),
+        ) as progress:
+            all_files = os.listdir(self.results_dir)
+            task = progress.add_task("[red]Merging", total=len(all_files))
+
+            with open(path, "w") as out_handle:
+                for i, filename in enumerate(all_files):
+                    with open(os.path.join(self.results_dir, filename), "r") as in_handle:
+                        shutil.copyfileobj(in_handle, out_handle)
+                        progress.update(task, completed=i+1)
